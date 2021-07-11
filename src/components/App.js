d97def0194607e0affcaf3efee7379262ec7ccd8
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-11 11:09:26 +0300

    Feat(bookstore): add BooksList and BooksForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,11 @@
+import BooksForm from "../containers/BooksForm";
+import BooksList from "../containers/BooksList";
+
 const App = () => {
-  return <div className="App">Book Store</div>;
+  return <div className="App">
+    <BooksList />
+    <BooksForm />
+  </div>;
 };
 
 export default App;
