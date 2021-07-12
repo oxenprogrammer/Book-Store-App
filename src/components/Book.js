dc1608ca99d54ec69abcd8fe06b725569b2404e9
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-12 14:54:52 +0300

    Feat(bookstore): add table row

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,26 @@
-const Book = () => {
-  return <div className="Book">Book Presentational Component</div>;
-};
+import PropTypes from 'prop-types';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 
-export default Book;
+export const Book = ({ id, title, category, pages, read }) => (
+  <TableRow>
+    <TableCell>{id}</TableCell>
+    <TableCell>{title}</TableCell>
+    <TableCell>{category}</TableCell>
+    <TableCell>{pages}</TableCell>
+    <TableCell>{read}</TableCell>
+  </TableRow>
+);
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    category: PropTypes.string,
+    pages: PropTypes.number,
+    read: PropTypes.bool,
+  }),
+};
+Book.defaultProps = {
+  book: null,
+};
