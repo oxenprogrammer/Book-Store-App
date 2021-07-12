9b78d869963bb6769871b8f604482d354323f517
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-12 20:45:37 +0300

    Feat(bookstore): update book reducer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,18 +1,17 @@
 import { ADD_BOOK, REMOVE_BOOK } from "../actions";
 
 export const books = (state = [], action) => {
+  const book = {
+    bookId:Math.floor(Math.random() * 1000),
+    read: "No",
+  }
   switch (action.type) {
-    case ADD_BOOK:
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 1000),
-          title: action.Book.title,
-          category: action.Book.category,
-          pages: action.Book.pages,
-          read: "No",
-        },
-      ];
+    case ADD_BOOK:{
+      let {id, title, pages, read, category } = action.payload;
+      id = book.bookId;
+      read = book.read;
+      return [...state, {id, read, title, pages, category}];
+    }
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.id);
     default:
