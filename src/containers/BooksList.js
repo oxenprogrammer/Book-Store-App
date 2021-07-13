a3c413fff9e6a280f3fa05cb4bd1bf2ce104c861
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-13 09:58:04 +0300

    Feat(bookstore): add book action

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,3 +1,4 @@
+import { Fragment, useState } from "react";
 import {
   Table,
   TableBody,
@@ -7,37 +8,92 @@ import {
   TableRow,
 } from "@material-ui/core";
 
+import { ActionButton } from "./ActionButton";
 import { Book } from "../components/Book";
+import CloseIcon from "@material-ui/icons/Close";
+import { ConfirmDialog } from "./ConfirmDialog";
 import Paper from "@material-ui/core/Paper";
 import { connect } from "react-redux";
 
 const BooksList = ({ books }) => {
+  const [confirmDialog, setConfirmDialog] = useState({
+    isOpen: false,
+    title: "",
+    subTitle: "",
+  });
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Title</TableCell>
-            <TableCell>Category</TableCell>
-            <TableCell>Pages</TableCell>
-            <TableCell>Completed</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {books.map((book) => (
-            <Book
-              key={book.id}
-              id={book.id}
-              title={book.title}
-              category={book.category}
-              pages={book.pages}
-              read={book.read}
-            />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Fragment>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Category</TableCell>
+              <TableCell>Pages</TableCell>
+              <TableCell>Completed</TableCell>
+              <TableCell>Remove</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {books.map((book) => (
+              <TableRow key={book.id}>
+                <Book
+                  key={book.id}
+                  id={book.id}
+                  title={book.title}
+                  category={book.category}
+                  pages={book.pages}
+                  read={book.read}
+                />
+                <TableCell>
+                  <ActionButton
+                    color="secondary"
+                    onClick={() => {
+                      setConfirmDialog({
+                        isOpen: true,
+                        title: "Are You Sure You want to Remove this Book?",
+                        subTitle: "You cannot undo this change",
+                      });
+                    }}
+                  >
+                    <CloseIcon fontSize="small" />
+                  </ActionButton>
+                </TableCell>
+              </TableRow>
+            ))}
+            {/* {books.map((book) => (
+              <Book
+                key={book.id}
+                id={book.id}
+                title={book.title}
+                category={book.category}
+                pages={book.pages}
+                read={book.read}
+              />
+            ))}
+            <TableCell>
+              <ActionButton
+                color="secondary"
+                onClick={() => {
+                  setConfirmDialog({
+                    isOpen: true,
+                    title: "Are You Sure You want to Delete Patient?",
+                    subTitle: "You cannot undo this change",
+                  });
+                }}
+              >
+                <CloseIcon fontSize="small" />
+              </ActionButton>
+            </TableCell> */}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <ConfirmDialog
+        confirmDialog={confirmDialog}
+        setConfirmDialog={setConfirmDialog}
+      />
+    </Fragment>
   );
 };
 
