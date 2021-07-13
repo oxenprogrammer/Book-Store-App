5895c9b756956267bc31ba004a3c82ee9f7b2157
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-13 13:01:42 +0300

    Feat(bookstore): add notofication snackbar

---
+++ src/containers/notification.js
@@ -0,0 +1,33 @@
+import { Snackbar, makeStyles } from '@material-ui/core';
+
+import Alert from '@material-ui/lab/Alert';
+import React from 'react';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    top: theme.spacing(18),
+  },
+}));
+
+export const notification = props => {
+  const { notify, setNotify } = props;
+  const classes = useStyles();
+  const handleClose = () => {
+    setNotify({
+      ...notify,
+      isOpen: false,
+    });
+  };
+  return (
+    <Snackbar
+      className={classes.root}
+      open={notify.isOpen}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
+      <Alert severity={notify.type} onClose={handleClose}>
+        {notify.message}
+      </Alert>
+    </Snackbar>
+  );
+};
