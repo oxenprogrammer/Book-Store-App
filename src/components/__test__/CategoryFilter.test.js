bccee263921b30c00a89949b57e3775b5d63c914
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-15 23:01:54 +0300

    Test(bookstore): add category filter

---
+++ src/components/__test__/CategoryFilter.test.js
@@ -0,0 +1,26 @@
+import { CategoryFilter } from "../CategoryFilter";
+import { unmountComponentAtNode } from "react-dom";
+import React from "react";
+import { act } from "react-dom/test-utils";
+import { renderWithState } from "../../utils/testHelper";
+
+describe("Header Component", () => {
+  let container = null;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  it("should render the Navbar", () => {
+    act(() => {
+      renderWithState(<CategoryFilter handleChange={() => console.log('works')} />, container);
+    });
+    expect(container).toBeDefined();
+  });
+});
