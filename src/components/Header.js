32eaba30160fb77aaf2e5eeb1076f3d1bf4dba44
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-15 14:11:38 +0300

    Feat(bookstore): make navbar fully responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 /* eslint-disable no-unused-vars */
 import {
   AppBar,
+  Button,
+  IconButton,
   Link,
+  ListItemIcon,
+  Menu,
+  MenuItem,
   Toolbar,
   Typography,
   makeStyles,
@@ -9,11 +14,22 @@ import {
   useTheme,
 } from "@material-ui/core";
 import React, { Fragment, memo } from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Link as mobileLink,
+} from "react-router-dom";
 
 import App from "./App";
+import BookmarksIcon from "@material-ui/icons/Bookmarks";
 import Categories from "./Categories";
+import HomeIcon from "@material-ui/icons/Home";
 import Icon from "@material-ui/core/Icon";
+// IMPORTING ICONS
+import MenuIcon from "@material-ui/icons/Menu";
+import PersonIcon from "@material-ui/icons/Person";
+import SchoolIcon from "@material-ui/icons/School";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -29,8 +45,11 @@ const useStyles = makeStyles(() => ({
     fontWeight: "bolder",
     flexGrow: 1,
     "@media(max-width: 678px)": {
-      padding: '0 1rem',
+      padding: "0 1rem",
       fontSize: "1.2rem",
+    },
+    "@media(max-width: 360px)": {
+      padding: "0",
     },
   },
   link: {
@@ -48,13 +67,18 @@ const useStyles = makeStyles(() => ({
   user: {
     color: "#0290ff",
   },
+  menu: {
+    color: "#0290ff",
+  },
 }));
 
 // eslint-disable-next-line react/display-name
 const Header = memo(() => {
+  const [anchor, setAnchor] = React.useState(null);
+  const open = Boolean(anchor);
   const classes = useStyles();
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
   const handleMenu = (event) => {
     setAnchor(event.currentTarget);
   };
@@ -63,20 +87,73 @@ const Header = memo(() => {
       <main>
         <AppBar position="static">
           <Toolbar className={classes.root} variant="dense">
-              <Typography variant="h6" color="inherit">
-                <Link className={classes.bookStoreCMS} href="#">
-                  BookStore CMS
-                </Link>
-              </Typography>
-              <Typography>
-                <Link className={classes.link} href="/">
-                  Books
-                </Link>
-                <Link className={classes.link} href="/categories">
-                  Categories
-                </Link>
-              </Typography>
-            <Icon className={["fas fa-user", classes.user]} />
+            {isMobile ? (
+              <>
+                <Typography variant="h6" color="inherit">
+                  <Link className={classes.bookStoreCMS} href="#">
+                    BookStore CMS
+                  </Link>
+                </Typography>
+                <IconButton
+                  color="textPrimary"
+                  className={classes.menuButton}
+                  edge="start"
+                  aria-label="menu"
+                  onClick={handleMenu}
+                >
+                  <MenuIcon className={classes.menu} />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchor}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  KeepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={open}
+                >
+                  <MenuItem
+                    onClick={() => setAnchor(null)}
+                    component={mobileLink}
+                    to="/"
+                  >
+                    <ListItemIcon></ListItemIcon>
+                    <Typography variant="h6">Books</Typography>
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => setAnchor(null)}
+                    component={mobileLink}
+                    to="/categories"
+                  >
+                    <ListItemIcon></ListItemIcon>
+                    <Typography variant="h6">Categories</Typography>
+                  </MenuItem>
+                </Menu>
+                <Icon className={["fas fa-user", classes.user]} />
+              </>
+            ) : (
+              <>
+                <Typography variant="h6" color="inherit">
+                  <Link className={classes.bookStoreCMS} href="#">
+                    BookStore CMS
+                  </Link>
+                </Typography>
+                <Typography>
+                  <Link className={classes.link} href="/">
+                    Books
+                  </Link>
+                  <Link className={classes.link} href="/categories">
+                    Categories
+                  </Link>
+                </Typography>
+                <Icon className={["fas fa-user", classes.user]} />
+              </>
+            )}
           </Toolbar>
         </AppBar>
         <Switch>
